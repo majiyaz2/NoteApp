a5cf01071744554ac927a00f8c1135ecfe2767fe
Andile Mhlongo <majiyaz2@gmail.com>
2024-03-15 17:30:59 +0200

    Attacked headers to requests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Pages from "./pages";
 import GlobalStyle from "./components/GlobalStyle";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider,createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 const uri = "http://localhost:4000/api";
 
+const httpLink = createHttpLink({uri})
 const cache = new InMemoryCache();
 
+const authLink = setContext((_, {headers}) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+
 const client = new ApolloClient({
-  uri,
+  link: authLink.concat(httpLink),
   cache,
+  resolvers: {},
   connectToDevTools:true
 });
 
