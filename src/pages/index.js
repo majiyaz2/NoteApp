455767b6142c9046bf6823eeeb9f093331b03e30
Andile Mhlongo <majiyaz2@gmail.com>
2024-03-15 22:29:25 +0200

    Added dynamic links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route,Navigate} from 'react-router-dom';
+import { useQuery, gql } from "@apollo/client";
+
 import Home from "./home";
 import MyNotes from "./mynotes";
 import Favorites from "./favourites";
 import Layout from "../components/Layout";
 import NotePage from "./note";
 import SignUp from "./signup";
+import SignIn from "./signin";
+import { resultKeyNameFromField } from "@apollo/client/utilities";
+
+
+const IS_LOGGED_IN = gql`
+query IsLoggedIn {
+    isLoggedIn @client
+  }
+`
+
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    const {loading, error, data} = useQuery(IS_LOGGED_IN);
+
+    if(data){
+        return data.isLoggedIn === true ? (
+
+            <Component {...rest}/>
+
+        ):(
+            <Navigate to="/signin" replace/>
+        )
+    }
+
+
+
+}
 
 const Pages = () => {
     return (
@@ -13,10 +42,11 @@ const Pages = () => {
             <Layout>
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
-                    <Route  path="/mynotes" element={<MyNotes/>}/>
-                    <Route  path="/favorites" element={<Favorites/>}/>
+                    <Route  path="/mynotes" element={<PrivateRoute component={MyNotes}/>}/>
+                    <Route  path="/favorites" element={<PrivateRoute component={Favorites}/>}/>
                     <Route  path="/note/:id" element={<NotePage/>}/>
                     <Route  path="/signup" element={<SignUp/>}/>
+                    <Route  path="/signin" element={<SignIn/>}/>
                 </Routes>
             </Layout>
         </BrowserRouter> 
